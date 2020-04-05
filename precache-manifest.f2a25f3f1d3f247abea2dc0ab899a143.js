5d53c9bc11e89708087eff9beca492d4504f414e
Lawrence Snead <Lawrence.Snead@gmail.com>
2020-04-04 22:17:02 -0400

    Updates

---
+++ precache-manifest.f2a25f3f1d3f247abea2dc0ab899a143.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a83f66e0ae196cfcbdda0f6c11d29a83",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "c3f177cee669db027c89",
+    "url": "/React-Portfolio/static/js/2.10e3f846.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Portfolio/static/js/2.10e3f846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d81eb2306e8bdec68c45",
+    "url": "/React-Portfolio/static/js/main.47f3b29f.chunk.js"
+  },
+  {
+    "revision": "c4576b65a865a712d736",
+    "url": "/React-Portfolio/static/js/runtime-main.bc73a675.js"
+  }
+]);
